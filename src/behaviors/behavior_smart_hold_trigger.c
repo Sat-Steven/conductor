3ca4b66d7151cd4b22b3f69565834e86f351d616
Sat-Steven <ymp1548@gmail.com>
2026-03-11 10:17:07 +0900

    Refactor smart hold trigger to use encoded values

--- src/behaviors/behavior_smart_hold_trigger.c
+++ src/behaviors/behavior_smart_hold_trigger.c
@@ -5,11 +5,7 @@
 #include <zephyr/kernel.h>
 #include <drivers/behavior.h>
 #include <zmk/behavior.h>
-
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-#include <zmk/hid.h>
-#include <dt-bindings/zmk/hid_usage.h>
-#endif
+#include <zmk/events/keycode_state_changed.h>
 
 struct smart_hold_trigger_config {
     int wait_time_ms;
@@ -17,20 +13,12 @@ struct smart_hold_trigger_config {
 
 struct smart_hold_trigger_data {
     bool active;
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    uint32_t hold_usage;
-    uint32_t trigger_usage;
+    uint32_t hold_encoded;
+    uint32_t trigger_encoded;
     struct k_work_delayable release_work;
-#endif
 };
 
 static struct smart_hold_trigger_data smart_state;
-
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-
-static uint32_t param_to_usage(uint32_t param) {
-    return ZMK_HID_USAGE(HID_USAGE_KEY, param);
-}
 
 static void release_hold_work_handler(struct k_work *work) {
     ARG_UNUSED(work);
@@ -39,44 +27,42 @@ static void release_hold_work_handler(struct k_work *work) {
         return;
     }
 
-    zmk_hid_release(smart_state.hold_usage);
-    smart_state.active = false;
-    smart_state.hold_usage = 0;
-    smart_state.trigger_usage = 0;
-}
+    raise_zmk_keycode_state_changed_from_encoded(
+        smart_state.hold_encoded, false, k_uptime_get());
 
-#endif
+    smart_state.active = false;
+    smart_state.hold_encoded = 0;
+    smart_state.trigger_encoded = 0;
+}
 
 static int smart_hold_trigger_press(struct zmk_behavior_binding *binding,
                                     struct zmk_behavior_binding_event event) {
-    ARG_UNUSED(event);
-
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
     const struct smart_hold_trigger_config *config = dev->config;
 
-    uint32_t hold_usage = param_to_usage(binding->param1);
-    uint32_t trigger_usage = param_to_usage(binding->param2);
+    uint32_t hold_encoded = binding->param1;
+    uint32_t trigger_encoded = binding->param2;
 
-    if (!smart_state.active || smart_state.hold_usage != hold_usage) {
+    if (!smart_state.active || smart_state.hold_encoded != hold_encoded) {
         if (smart_state.active) {
-            zmk_hid_release(smart_state.hold_usage);
+            raise_zmk_keycode_state_changed_from_encoded(
+                smart_state.hold_encoded, false, event.timestamp);
         }
 
         smart_state.active = true;
-        smart_state.hold_usage = hold_usage;
-        smart_state.trigger_usage = trigger_usage;
+        smart_state.hold_encoded = hold_encoded;
+        smart_state.trigger_encoded = trigger_encoded;
 
-        zmk_hid_press(hold_usage);
+        raise_zmk_keycode_state_changed_from_encoded(
+            hold_encoded, true, event.timestamp);
     }
 
-    zmk_hid_press(trigger_usage);
-    zmk_hid_release(trigger_usage);
+    raise_zmk_keycode_state_changed_from_encoded(
+        trigger_encoded, true, event.timestamp);
+    raise_zmk_keycode_state_changed_from_encoded(
+        trigger_encoded, false, event.timestamp);
 
     k_work_reschedule(&smart_state.release_work, K_MSEC(config->wait_time_ms));
-#else
-    ARG_UNUSED(binding);
-#endif
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -107,20 +93,13 @@ static const struct behavior_driver_api smart_hold_trigger_driver_api = {
                             CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                    \
                             &smart_hold_trigger_driver_api);
 
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 static int smart_hold_trigger_global_init(void) {
     smart_state.active = false;
-    smart_state.hold_usage = 0;
-    smart_state.trigger_usage = 0;
+    smart_state.hold_encoded = 0;
+    smart_state.trigger_encoded = 0;
     k_work_init_delayable(&smart_state.release_work, release_hold_work_handler);
     return 0;
 }
-#else
-static int smart_hold_trigger_global_init(void) {
-    smart_state.active = false;
-    return 0;
-}
-#endif
 
 SYS_INIT(smart_hold_trigger_global_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
 
